90914c817d47544c1aac9685c79745809ec63f52
rjcorflo <rj.corchero@gmail.com>
2016-01-30 11:20:03 +0100

    Bug fixing.

--- src/classes/GestorSimulaciones.php
+++ src/classes/GestorSimulaciones.php
@@ -2,65 +2,150 @@
 
 class GestorSimulaciones
 {
-  const SIMULACION_UNO = 1;
-  const SIMULACION_DOS = 2;
-  const SIMULACION_TRES = 3;
-  const SIMULACION_CUATRO = 4;
-  const SIMULACION_CINCO = 5;
+  const SIM_UNO = 1;
+  const SIM_DOS = 2;
+  const SIM_TRES = 3;
+  const SIM_CUATRO = 4;
+  const SIM_CINCO = 5;
 
-  /* @var ListaSimulaciones */
-  private $listaSimulaciones;
+  /* @var array */
+  private $idsSimulacionesCargadas;
+  /* @var stdClass $usuarioActual */
+  private $usuarioActual;
+  /* @var ListaSimulaciones[] Un array de la form $uid => ListaSimulaciones */
+  private $arrayListaSimulacionesByUser;
 
-  /**
-   * @param array $idsSimulacionesACargar
-   * @param null|int $uid
-   */
-  public function __construct($idsSimulacionesACargar, $uid = null)
+  public function __construct(array $idsSimulacionesACargar, stdClass $usuarioActual)
   {
-    $this->listaSimulaciones = new ListaSimulaciones();
-    foreach ($idsSimulacionesACargar as $id) {
-      $this->listaSimulaciones->add(new Simulacion($id, $uid));
+    $usuarios = entity_load('user');
+
+    foreach ($usuarios as $usuario) {
+      $this->arrayListaSimulacionesByUser[$usuario->uid] = new ListaSimulaciones();
+      foreach ($idsSimulacionesACargar as $id) {
+        $this->arrayListaSimulacionesByUser[$usuario->uid]->add(new Simulacion($id, $usuario));
+      }
     }
 
-    $this->uid = $uid;
+    $this->setUsuarioActual($usuarioActual);
   }
 
   /**
-   * @return int|null
+   * @return array
    */
-  public function getUid()
+  public function getIdsSimulacionesCargadas()
   {
-    return $this->uid;
+    return $this->idsSimulacionesCargadas;
   }
 
   /**
-   * @param int|null $uid
+   * @param array $idsSimulacionesCargadas
    */
-  public function setUid($uid)
+  public function setIdsSimulacionesCargadas(array $idsSimulacionesCargadas)
   {
-    $this->uid = $uid;
+    $this->idsSimulacionesCargadas = $idsSimulacionesCargadas;
   }
 
   /**
-   * @return ListaSimulaciones
+   * @return stdClass
    */
-  public function getListaSimulaciones()
+  public function getUsuarioActual()
   {
-    return $this->listaSimulaciones;
+    return $this->usuarioActual;
   }
 
+  /**
+   * @param stdClass $usuarioActual
+   */
+  public function setUsuarioActual(stdClass $usuarioActual)
+  {
+    $this->usuarioActual = $usuarioActual;
+  }
 
   /**
-   * @return ListaPartidas Listado de partidas de este usuario para esta simulación (o para todas)
+   * @return ListaSimulaciones[]
    */
-  function retrieveAllSimulationsPartidas()
+  public function getArrayListaSimulacionesByUser()
+  {
+    return $this->arrayListaSimulacionesByUser;
+  }
+
+  /**
+   * @param stdClass $usuario
+   * @return ListaSimulaciones La lista de las simulaciones cargadas para el usuario pasado.
+   * @throws LogicException Si no existe el usuario.
+   */
+  public function getListaSimulacionesByUser(stdClass $usuario) {
+    if (array_key_exists($usuario->uid, $this->getArrayListaSimulacionesByUser())) {
+      return $this->getArrayListaSimulacionesByUser()[$usuario->uid];
+    } else {
+      throw new LogicException("No se han cargado las partidas de este usuario o no existe.");
+    }
+  }
+
+  /**
+   * @return ListaSimulaciones La lista de las simulaciones cargadas para el usuario actual de la sesión.
+   * @throws LogicException Si no existe el usuario.
+   */
+  public function getListaSimulacionesUsuarioActual() {
+    return $this->getListaSimulacionesByUser($this->getUsuarioActual());
+  }
+
+  /**
+   * @return ListaPartidas Listado de todas las partidas de todos los usuarios.
+   */
+  public function retrieveAllPartidas()
   {
     $listaPartidas = new ListaPartidas();
 
-    foreach ($this->listaSimulaciones as $simulacion) {
-      $listaPartidas->mergeList($simulacion->getListaPartidas());
+    foreach ($this->getArrayListaSimulacionesByUser() as $listaSimulaciones) {
+      foreach ($listaSimulaciones as $simulacion) {
+        $listaPartidas->mergeList($simulacion->getListaPartidas());
+      }
     }
 
     return $listaPartidas;
+  }
+
+  /**
+   * @param int $idSimulation El id de la simulación para la que recuperar las partidas de los usuarios.
+   * @return ListaPartidas Lista de todas las partidas de esa simulación de todos los usuarios.
+   */
+  public function retrieveAllPartidasByIdSimulacion($idSimulation)
+  {
+    $listaPartidas = new ListaPartidas();
+
+    foreach ($this->getArrayListaSimulacionesByUser() as $listaSimulaciones) {
+      foreach ($listaSimulaciones as $simulacion) {
+        if (in_array($idSimulation, $this->getIdsSimulacionesCargadas()) && $simulacion->getIdSimulacion() == $idSimulation) {
+          $listaPartidas->mergeList($simulacion->getListaPartidas());
+        }
+      }
+    }
+
+    return $listaPartidas;
+  }
+
+  /**
+   * @param stdClass $usuario
+   * @return ListaPartidas Lista de todas las partidas de ese usuario de todas las simulaciones cargadas en el gestor.
+   */
+  public function retrieveAllPartidasByUser(stdClass $usuario)
+  {
+    $listaPartidas = new ListaPartidas();
+
+    if (array_key_exists($usuario->uid, $this->getArrayListaSimulacionesByUser())) {
+      foreach ($this->getArrayListaSimulacionesByUser()[$usuario->uid] as $simulacion) {
+        $listaPartidas->mergeList($simulacion->getListaPartidas());
+      }
+    }
+
+    return $listaPartidas;
+  }
+
+  /**
+   * @return ListaPartidas Lista de todas las partidas del usuario actual de todas las simulaciones cargadas en el gestor.
+   */
+  public function retrieveAllPartidasUsuarioActual() {
+    return $this->retrieveAllPartidasByUser($this->getUsuarioActual());
   }
 } 
