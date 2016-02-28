29483c5c651f3af9e2741de341ba6a1458dc94d0
rjcorflo <rj.corchero@gmail.com>
2016-02-28 15:43:49 +0100

    Adding user infractions analysis.

--- sites/all/modules/custom/rjsimulador/src/classes/ListaUsuariosDataRetriever.php
+++ sites/all/modules/custom/rjsimulador/src/classes/ListaUsuariosDataRetriever.php
@@ -71,4 +71,50 @@ class ListaUsuariosDataRetriever {
 
     return $listaPartidas;
   }
+
+  /**
+   * Devuelve todas las infracciones de todas las partidas de la lista de usuarios para una simulación en concreto.
+   * @param int $idSimulacion ID de la Simulación para la que recuperar las infracciones.
+   * @return \ListaInfracciones Lista de infracciones para la simulación pasada.
+   */
+  public function retrieveAllInfraccionesByIdSimulacion($idSimulacion) {
+    $listaInfracciones = new ListaInfracciones();
+    // Recuperamos todas las infracciones de una simulación en concreto
+    foreach ($this->retrieveAllPartidasByIdSimulacion($idSimulacion) as $partida) {
+      $listaInfracciones->mergeList($partida->getListaInfracciones());
+    }
+
+    return $listaInfracciones;
+  }
+
+  /**
+   * Devuelve todas las infracciones de un cierto tipo de la lista de ususarios para las simulación pasada.
+   * @param int $idInfraccion El ID de la infracción a recuperar.
+   * @param int $idSimulacion ID de la simulación para la que recuperar las infracciones.
+   * @return \ListaInfracciones Lista de infracciones del tipo pasado para la simulación.
+   */
+  public function retrieveAllInfraccionesByTypeAndIdSimulacion($idInfraccion, $idSimulacion) {
+    $listaInfracciones = $this->retrieveAllInfraccionesByIdSimulacion($idSimulacion);
+    $arrayIdsInfracciones = array($idInfraccion);
+    // Devolvemos la lista de infracciones filtradas por el IdInfraccion
+    return $listaInfracciones->filterBy(new FilterByEquality($arrayIdsInfracciones, FilterByEquality::INFRACCION_ID));
+  }
+
+  /**
+   * Devuelve la media de infracciones de un cierto tipo cometidas por partida de lal lista de usuarios para la simulación pasada.
+   * @param int $idInfraccion El ID de la infracción.
+   * @param int $idSimulation El ID dela simulación.
+   * @return float|int Media de infracciones por partida del grupo de usuarios.
+   */
+  public function getAverageInfraccionesByPartida($idInfraccion, $idSimulation) {
+    // Recuperamos total de partidas de una simulacion
+    $totalPartidasSimulacion = $this->retrieveAllPartidasByIdSimulacion($idSimulation)->count();
+    // Recuperamos total de infracciones de un ciertto tipo por simulacion
+    $totalInfraccionesSimulacion = $this->retrieveAllInfraccionesByTypeAndIdSimulacion($idInfraccion, $idSimulation)->count();
+
+    // Si el total de partidas es 0 devolvemos 0 evitando la división
+    $mediaInfraccionesPorPartida = $totalPartidasSimulacion > 0 ? $totalInfraccionesSimulacion/$totalPartidasSimulacion : 0;
+
+    return $mediaInfraccionesPorPartida;
+  }
 }
