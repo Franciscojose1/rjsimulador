519a2b3381e6e71f17065334b6c7dfa46b17342f
rjcorflo <rj.corchero@gmail.com>
2016-03-05 23:38:22 +0100

    Adding Config Page to Module.

--- src/classes/Grupos.php
+++ src/classes/Grupos.php
@@ -1,6 +1,11 @@
 <?php
 
 class Grupos {
+  private static $dataMinGroups = 1;
+  private static $dataMaxGroups = 6;
+  private static $infractionsMinGroups = 0;
+  private static $infractionsMaxGroups = 2;
+
   /* @var array $GruposEdad Array con los grupos de edad considerados actualmente. */
   private static $GruposEdad = array(
     1 => array(FilterByInterval::DESDE => 0, FilterByInterval::HASTA => 30),
@@ -21,6 +26,22 @@ class Grupos {
     2 => array(FilterByInterval::DESDE => 30000, FilterByInterval::HASTA => 50000),
     3 => array(FilterByInterval::DESDE => 50000, FilterByInterval::HASTA => 999999)
   );
+
+  public static function getDataMinGroups() {
+    return self::$dataMinGroups;
+  }
+
+  public static function getDataMaxGroups() {
+    return self::$dataMaxGroups;
+  }
+
+  public static function getInfractionsMinGroups() {
+    return self::$infractionsMaxGroups;
+  }
+
+  public static function getInfractionsMaxGroups() {
+    return self::$infractionsMaxGroups;
+  }
 
   public static function getGruposEdad() {
     return variable_get('rjsimulador_grupos_edad', self::$GruposEdad);
